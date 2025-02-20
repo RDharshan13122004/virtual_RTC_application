507594408609ccee35058dae64f0d8a7238c2393
RDharshan13122004 <rdharshansrinivas@gmail.com>
2025-02-20 23:55:43 +0530

    test 2

--- m_server.py
+++ m_server.py
@@ -28,7 +28,7 @@ def mix_audio(audio_data_list):
     min_length = min(len(arr) for arr in audio_arrays)
     audio_arrays = [arr[:min_length] for arr in audio_arrays]
 
-    mixed_audio = np.mean(audio_arrays, axis=0).astypes(np.int16)
+    mixed_audio = np.mean(audio_arrays, axis=0).astype(np.int16)
 
     return mixed_audio.tobytes()
 
@@ -40,7 +40,7 @@ def video_stream_handler(vid_client_socket,client_assign_id):
             if not frame_size_data:
                 break
 
-        frame_size = struct.unpack(frame_size_data)[0]
+        frame_size = struct.unpack("Q",frame_size_data)[0]
         frame_data = b""
 
         while len(frame_data) < frame_size:
@@ -51,7 +51,7 @@ def video_stream_handler(vid_client_socket,client_assign_id):
             frame_data += packet
 
             try:
-                decompressed_data = zlib.compress(frame_data)
+                decompressed_data = zlib.decompress(frame_data)
             except zlib.error as e:
                 continue
         
